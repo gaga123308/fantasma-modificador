e4b19b4fcdf37a7b089abdaf7521f9e60137f3dc
Gagadev <gamerissac545@gmail.com>
2026-08-31 02:48:46 -0300

    Update fantasma.py

--- fantasma.py
+++ fantasma.py
@@ -4,7 +4,7 @@ import os
 def criar_painel():
     os.system("clear")
     
-    # 1. EXIBE TODAS AS INFORMAÇÕES E CANAIS ENQUANTO RODA O SCRIPT
+    # 1. EXIBE TODAS AS INFORMAÇÕES E REDES SOCIAIS ENQUANTO RODA O SCRIPT
     print "\033[31m  ______             _                    \033[0m"
     print "\033[33m |  ____|           | |                   \033[0m"
     print "\033[32m | |__ __ _ _ __ ___| |_ __ _ _ __ ___    \033[0m"
@@ -29,16 +29,27 @@ def criar_painel():
     usuario_custom = raw_input("Digite o nome de usuario desejado (ex: fantasma): ")
     host_custom = raw_input("Digite o nome da maquina/host desejado (ex: localhost): ")
     
-    print "\nEscolha o estilo de cor para o prompt e textos:"
-    print "1 - Cores fixas"
-    print "2 - RGB / Colorido (Arco-íris dinâmico)"
-    tipo_cor = raw_input("Digite 1 ou 2: ")
+    # Pergunta sobre o robô/arte adicional
+    quer_robo = raw_input("Quer colocar um robo/arte (sim ou nao)? ").strip().lower()
+    tipo_robo = ""
+    if quer_robo in ["sim", "s", "y", "yes"]:
+        print "\nTipos disponiveis: [android] / [kali] / [fantasma]"
+        tipo_robo = raw_input("Qual tipo de robo voce quer? ").strip().lower()
 
     home_dir = os.environ.get("HOME", "/data/data/com.termux/files/home")
     bashrc_path = os.path.join(home_dir, ".bashrc")
     motd_path = os.path.join(home_dir, "../usr/etc/motd")
 
-    # 2. O QUE FICA SALVO PARA APARECER SEMPRE QUE ENTRA/ SAI DO TERMUX (Banner limpo no topo)
+    # Define o desenho do robô/arte com base na escolha
+    arte_robo = ""
+    if tipo_robo == "android":
+        arte_robo = """\033[32m       \\x\\x\\x/x/x/       \n       [o]   [o]       \n      /   \\_/   \\      \n     |           |     \n     |___|   |___|     \n     \\_|_|   |_|_/     \033[0m\n"""
+    elif tipo_robo == "kali":
+        arte_robo = """\033[37m         /\\                               \n        /  \\  /\\                          \n       / /\\ \\/  \\                         \n      / /  \\/\\   \\                        \n     /_/      \\___\\                       \033[0m\n"""
+    elif tipo_robo == "fantasma":
+        arte_robo = """\033[37m       .-.        \n      (o.o)       \n       |=|        \n      /   \\       \n     (_/ \\_)      \033[0m\n"""
+
+    # 2. CONTEÚDO FIXO PARA O TOPO DO TERMUX (MOTD) COM O BANNER E O ROBÔ ESCOLHIDO
     motd_conteudo = """\033[31m  ______             _                    \033[0m
 \033[33m |  ____|           | |                   \033[0m
 \033[32m | |__ __ _ _ __ ___| |_ __ _ _ __ ___    \033[0m
@@ -49,17 +60,11 @@ def criar_painel():
 --------------------------------------------------
 \033[1;33mCriador: gagadev and fantasma team\033[0m
 --------------------------------------------------
-"""
+""" + arte_robo
 
-    # 3. CONFIGURAÇÃO DO PROMPT (RGB com várias cores misturadas nas palavras)
-    if tipo_cor == "2":
-        # Prompt totalmente RGB colorido em cada parte
-        ps1_config = 'export PS1="\\[\\e[31m\\](%s\\[\\e[33m\\]@%s)\\[\\e[32m\\]-\\[\\e[36m\\][\\w]\\[\\e[0m\\]\\n\\[\\e[35m\\$\\e[0m\\] "' % (usuario_custom, host_custom)
-    else:
-        cor_prompt = raw_input("Qual sera a cor fixa (ex: red, green, blue, cyan)? ")
-        cores = {"vermelho": "31", "verde": "32", "amarelo": "33", "azul": "34", "magenta": "35", "ciano": "36", "branco": "37"}
-        codigo_cor = cores.get(cor_prompt, "32")
-        ps1_config = 'export PS1="\\[\\e[1;%sm\\](%s@%s)-\\[\\e[0m\\]\\[\\e[1;34m\\][\\w]\\[\\e[0m\\]\\n\\$ "' % (codigo_cor, usuario_custom, host_custom)
+    # 3. PROMPT TOTALMENTE RGB (COLORIDO EM TODAS AS PARTES E NO TEXTO QUE VOCÊ DIGITA)
+    # Usa códigos ANSI para colorir o usuário, o host, o diretório e o caractere de comando ($)
+    ps1_config = 'export PS1="\\[\\e[1;31m\\](%s\\[\\e[1;33m\\]@%s)\\[\\e[1;32m\\]-\\[\\e[1;36m\\][\\w]\\[\\e[0m\\]\\n\\[\\e[1;35m\\$\\e[0m \\e[1;37m"' % (usuario_custom, host_custom)
 
     try:
         with open(motd_path, "w") as f:
@@ -69,8 +74,8 @@ def criar_painel():
             f.write(ps1_config + "\n")
 
         print ""
-        print "\033[1;32m[+] Sucesso! Painel configurado com sucesso!\033[0m"
-        print "Abra uma nova aba no Termux para ver o resultado completo!"
+        print "\033[1;32m[+] Sucesso! Painel com robo e RGB total configurado com sucesso!\033[0m"
+        print "Abra uma nova aba no Termux para conferir o resultado!"
     except Exception as e:
         print "Erro ao salvar configuracao:", e
 
