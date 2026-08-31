9e565fa59590ac7b3015999052b3439d5c0d271d
Gagadev <gamerissac545@gmail.com>
2026-08-31 02:43:03 -0300

    Update fantasma.py

--- fantasma.py
+++ fantasma.py
@@ -4,38 +4,41 @@ import os
 def criar_painel():
     os.system("clear")
     
-    print "\033[1;36m=== CONFIGURACAO DO FANTASMA MODIFICADOR ===\033[0m\n"
+    # 1. EXIBE TODAS AS INFORMAÇÕES E CANAIS ENQUANTO RODA O SCRIPT
+    print "\033[31m  ______             _                    \033[0m"
+    print "\033[33m |  ____|           | |                   \033[0m"
+    print "\033[32m | |__ __ _ _ __ ___| |_ __ _ _ __ ___    \033[0m"
+    print "\033[36m |  __/ _` | '__/ __| __/ _` | '_ ` _ \\   \033[0m"
+    print "\033[34m | | | (_| | |  \\__ \\ || (_| | | | | | |  \033[0m"
+    print "\033[35m |_|  \\__,_|_|  |___/\\__\\__,_|_| |_| |_|  \033[0m"
+    print "\033[1;36m       M O D I F I C A D O R              \033[0m"
+    print "--------------------------------------------------"
+    print "\033[1;33mCriador: gagadev and fantasma team\033[0m"
+    print "--------------------------------------------------"
+    print " [i] Discord dos desenvolvedores: https://discord.gg/szcg2ZaA5"
+    print " [i] Canal do YT (oficial): https://www.youtube.com/@gagadv2"
+    print " [i] TikTok dos desenvolvedores: tiktok.com/@fantasmahub"
+    print " [i] Roblox do desenvolvedor: https://www.roblox.com/share?code=65e7b03d3bb7974086b1af442cd5c6ab&type=Profile&source=ProfileShare&stamp=1788154189471"
+    print "--------------------------------------------------"
+    print " [!] Se tiver algum problema no painel, abre o ticket"
+    print "     no Discord do desenvolvedor!"
+    print "--------------------------------------------------"
+    print ""
+    
+    # Pergunta os dados ao usuário
     usuario_custom = raw_input("Digite o nome de usuario desejado (ex: fantasma): ")
     host_custom = raw_input("Digite o nome da maquina/host desejado (ex: localhost): ")
     
-    print "\nEscolha a cor do seu prompt:"
-    print "1 - Cores fixas (vermelho, verde, amarelo, azul, ciano, branco)"
-    print "2 - RGB / Colorido"
+    print "\nEscolha o estilo de cor para o prompt e textos:"
+    print "1 - Cores fixas"
+    print "2 - RGB / Colorido (Arco-íris dinâmico)"
     tipo_cor = raw_input("Digite 1 ou 2: ")
-
-    codigo_cor = "32"
-    usar_rgb_prompt = False
-
-    if tipo_cor == "2":
-        usar_rgb_prompt = True
-    else:
-        cor_prompt = raw_input("Qual sera a cor fixa (ex: red, green, blue, cyan)? ")
-        cores = {
-            "vermelho": "31", "red": "31",
-            "verde": "32", "green": "32",
-            "amarelo": "33", "yellow": "33",
-            "azul": "34", "blue": "34",
-            "magenta": "35", "purple": "35",
-            "ciano": "36", "cyan": "36",
-            "branco": "37", "white": "37"
-        }
-        codigo_cor = cores.get(cor_prompt, "32")
 
     home_dir = os.environ.get("HOME", "/data/data/com.termux/files/home")
     bashrc_path = os.path.join(home_dir, ".bashrc")
     motd_path = os.path.join(home_dir, "../usr/etc/motd")
 
-    # Mensagem colorida que fica estampada no topo sempre que abrir o Termux
+    # 2. O QUE FICA SALVO PARA APARECER SEMPRE QUE ENTRA/ SAI DO TERMUX (Banner limpo no topo)
     motd_conteudo = """\033[31m  ______             _                    \033[0m
 \033[33m |  ____|           | |                   \033[0m
 \033[32m | |__ __ _ _ __ ___| |_ __ _ _ __ ___    \033[0m
@@ -46,19 +49,16 @@ def criar_painel():
 --------------------------------------------------
 \033[1;33mCriador: gagadev and fantasma team\033[0m
 --------------------------------------------------
- [i] Discord dos desenvolvedores: https://discord.gg/szcg2ZaA5
- [i] Canal do YT (oficial): https://www.youtube.com/@gagadv2
- [i] TikTok dos desenvolvedores: tiktok.com/@fantasmahub
- [i] Roblox do desenvolvedor: https://www.roblox.com/share?code=65e7b03d3bb7974086b1af442cd5c6ab&type=Profile&source=ProfileShare&stamp=1788154189471
---------------------------------------------------
- [!] Se tiver algum problema no painel, abre o ticket
-     no Discord do desenvolvedor!
---------------------------------------------------
 """
 
-    if usar_rgb_prompt:
-        ps1_config = 'export PS1="\\[\\e[31m\\](%s@%s)-\\[\\e[32m\\][\\w]\\[\\e[0m\\]\\n\\$ "' % (usuario_custom, host_custom)
+    # 3. CONFIGURAÇÃO DO PROMPT (RGB com várias cores misturadas nas palavras)
+    if tipo_cor == "2":
+        # Prompt totalmente RGB colorido em cada parte
+        ps1_config = 'export PS1="\\[\\e[31m\\](%s\\[\\e[33m\\]@%s)\\[\\e[32m\\]-\\[\\e[36m\\][\\w]\\[\\e[0m\\]\\n\\[\\e[35m\\$\\e[0m\\] "' % (usuario_custom, host_custom)
     else:
+        cor_prompt = raw_input("Qual sera a cor fixa (ex: red, green, blue, cyan)? ")
+        cores = {"vermelho": "31", "verde": "32", "amarelo": "33", "azul": "34", "magenta": "35", "ciano": "36", "branco": "37"}
+        codigo_cor = cores.get(cor_prompt, "32")
         ps1_config = 'export PS1="\\[\\e[1;%sm\\](%s@%s)-\\[\\e[0m\\]\\[\\e[1;34m\\][\\w]\\[\\e[0m\\]\\n\\$ "' % (codigo_cor, usuario_custom, host_custom)
 
     try:
@@ -69,8 +69,8 @@ def criar_painel():
             f.write(ps1_config + "\n")
 
         print ""
-        print "\033[1;32m[+] Sucesso! Banner arco-íris configurado para aparecer sempre!\033[0m"
-        print "Abra uma nova aba no Termux para conferir!"
+        print "\033[1;32m[+] Sucesso! Painel configurado com sucesso!\033[0m"
+        print "Abra uma nova aba no Termux para ver o resultado completo!"
     except Exception as e:
         print "Erro ao salvar configuracao:", e
 
