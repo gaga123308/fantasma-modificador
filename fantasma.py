0372bdf1ba15b18feaeccf588aee350d2880b7ff
Gagadev <gamerissac545@gmail.com>
2026-08-31 02:32:39 -0300

    Update fantasma.py

--- fantasma.py
+++ fantasma.py
@@ -2,13 +2,38 @@
 import os
 
 def criar_painel():
-    print "===================================="
-    print "       FANTASMA MODIFICADOR"
-    print "===================================="
+    # Limpa a tela para o banner ocupar o topo
+    os.system("clear")
     
-    mensagem = raw_input("O que você quer que apareça na sua tela permanente? \n> ")
-    cor = raw_input("Qual será a cor da sua mensagem (ex: red, green, blue, yellow)? \n> ").lower()
+    # Banner grande que ocupa bastante espaço na tela
+    print "\033[1;36m"
+    print "  ______             _                    "
+    print " |  ____|           | |                   "
+    print " | |__ __ _ _ __ ___| |_ __ _ _ __ ___    "
+    print " |  __/ _` | '__/ __| __/ _` | '_ ` _ \\   "
+    print " | | | (_| | |  \\__ \\ || (_| | | | | | |  "
+    print " |_|  \\__,_|_|  |___/\\__\\__,_|_| |_| |_|  "
+    print "       M O D I F I C A D O R              "
+    print "\033[0m"
+    print "--------------------------------------------------"
+    print "\033[1;33mCriador: gagadev and fantasma team\033[0m"
+    print "--------------------------------------------------"
+    print " [i] Discord dos desenvolvedores: https://discord.gg/szcg2ZaA5"
+    print " [i] Canal do YT (oficial): https://www.youtube.com/@gagadv2"
+    print " [i] TikTok dos desenvolvedores: tiktok.com/@fantasmahub"
+    print " [i] Roblox do desenvolvedor: https://www.roblox.com/share?code=65e7b03d3bb7974086b1af442cd5c6ab&type=Profile&source=ProfileShare&stamp=1788154189471"
+    print "--------------------------------------------------"
+    print " [!] Se tiver algum problema no painel, abre o ticket"
+    print "     no Discord do desenvolvedor!"
+    print "--------------------------------------------------"
+    print ""
     
+    # Pergunta o nome personalizado para o prompt
+    usuario_custom = raw_input("Digite o nome de usuario desejado (ex: fantasma): ")
+    host_custom = raw_input("Digite o nome da maquina/host desejado (ex: localhost): ")
+    cor_prompt = raw_input("Qual sera a cor do seu prompt (ex: red, green, blue, cyan)? ")
+
+    # Mapeamento de cores
     cores = {
         "vermelho": "31", "red": "31",
         "verde": "32", "green": "32",
@@ -19,27 +44,33 @@ def criar_painel():
         "branco": "37", "white": "37"
     }
     
-    codigo_cor = cores.get(cor, "37")
-    
-    bloco_bash = """
-# --- Inicio do Painel Fantasma ---
-echo -e "\\033[1;%sm----------------------------------\\033[0m"
-echo -e "\\033[1;%sm %s \\033[0m"
-echo -e "\\033[1;%sm----------------------------------\\033[0m"
-# --- Fim do Painel Fantasma ---
-""" % (codigo_cor, codigo_cor, mensagem, codigo_cor)
+    codigo_cor = cores.get(cor_prompt, "32")
 
+    # Caminho do arquivo de configuração do bash no Termux
     home_dir = os.environ.get("HOME", "/data/data/com.termux/files/home")
     bashrc_path = os.path.join(home_dir, ".bashrc")
-    
+    motd_path = os.path.join(home_dir, "../usr/etc/motd")
+
+    # Remove a mensagem de boas-vindas padrão do Termux
     try:
-        arquivo = open(bashrc_path, "a")
-        arquivo.write(bloco_bash)
-        arquivo.close()
-        print "\n[Sucesso] Configuração aplicada com sucesso!"
-        print "Feche e abra o Termux novamente para ver a mensagem."
+        if os.path.exists(motd_path):
+            with open(motd_path, "w") as f:
+                f.write("")
     except Exception as e:
-        print "\n[Erro] Não foi possível alterar o arquivo:", e
+        print "Aviso ao limpar o motd:", e
+
+    # Cria o novo layout do prompt personalizado no .bashrc
+    ps1_config = 'export PS1="\\[\\e[1;%sm\\](%s@%s)-\\[\\e[0m\\]\\[\\e[1;34m\\][\\w]\\[\\e[0m\\]\\n\\$ "' % (codigo_cor, usuario_custom, host_custom)
+
+    try:
+        with open(bashrc_path, "w") as f:
+            f.write(ps1_config + "\n")
+        print ""
+        print "\033[1;32m[+] Sucesso! Painel configurado com as redes e prompt atualizado.\033[0m"
+        print "Abra uma nova aba no Termux para conferir o resultado!"
+    except Exception as e:
+        print "Erro ao salvar configuracao:", e
 
 if __name__ == "__main__":
     criar_painel()
+    
